110e12d93bc73bf6c6f2b530228510c21b55bd71
Aaron99u <josearon_@hotmail.com>
2023-05-30 13:25:28 -0600

    configurar carrito1

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isAsideOpen = !aside.classList.contains('inactive');
+
+    aside.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
